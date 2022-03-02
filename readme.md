0ef611d11402e4585dca20e4848046d2c389623c
Mathias Isaksen <mathiasleanderi@gmail.com>
2022-03-02 13:14:16 +0100

    Finishes most of documentation

---
+++ readme.md
@@ -0,0 +1,170 @@
+# poissimple
+
+poissimple is a JavaScript package that implements simple/naive [Poisson disk sampling (PDS)](https://www.jasondavies.com/poisson-disc/).
+It allows for the generation of a given number of points in any number of dimensions, with tileability as an option.
+
+If you require speed and efficiency, or features like varying sample density, it is recommended to use an alternative like [poisson-disk-sampling](https://github.com/kchapelier/poisson-disk-sampling) (which is a great package, and the main inspiration for the API of poissimple).
+
+## Installation
+The module is available on npm:
+
+```
+npm install poissimple
+```
+
+## Motivation
+While seemingly every other implementation of PDS requires the user to specify the minimum distance between samples, poissimple lets the user generate a given number of points.
+The minimum distance is computed using a heuristic that works reasonably well in 1, 2 and 3 dimensions.
+There are no guarantees for 4 dimensions and above.
+
+poissimple uses the brute-force approach, which requires computing the pairwise distances between every new candidate and the previously accepted points.
+This leads to a runtime of O(n²), where n is the number of points to be generated.
+While slower than the O(n) [Bridson algorithm](https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf), poissimple is useful in cases where a small, specified number of points is needed.
+
+To give an idea of the performance, the table below shows the average time needed to generate a certain number of points in dimensions 1 through 4
+(measured in milliseconds on a 2021 M1 MBP):
+
+<center>
+
+| n /  Dimensions  | 1 | 2 | 3 | 4 |
+| ------------- | ------------- | ------------- | ------------- | ------------- |
+| 10 | 0.6 | 0.5 | 0.4 | 0.6 |
+| 20 | 1.1 | 0.8 | 1 | 1.1 |
+| 50 | 3.2 | 2.9 | 2.5 | 1.6 |
+| 100 | 11.5 | 9.6 | 10.6 | 9.3 |
+| 200 | 37.4 | 38.8 | 41.2 | 31.9 |
+| 500 | 234 | 255 | 300 | 240 |
+| 1000 | 924 | 980 | 1209 | 1064 |
+| 2000 | 3658 | 4030 | 5001 | 4562 |
+| 4000 | 14522 | 16888 | 21677 | 19472 |
+
+</center>
+
+For n <= 200, poissimple is fast enough for many uses.
+
+## Examples of use
+
+### **One-dimensional points:**
+Maybe not the most useful feature, but nonetheless possible.
+The code below generates 20 points on the interval [0, 1]:
+
+``` js
+const ps = new Poissimple({
+    n: 20,
+    dimensions: 1,
+    extent: [0, 1]
+});
+console.log(ps.fill()); // Outputs array of values: [x1, x2, x3, ...]
+```
+
+<center>
+<img src="./docs/img/1d-example.png" width="70%"></img>
+</center>
+
+### **Two-dimensional points:**
+Next, we generate 200 two-dimensional points in the rectangle [-1, 1] × [0, 1], with `periodic` set to `true`.
+The distance between points are then computed as if the opposite horizontal and vertical boundaries are "connected/glued together", which ensures that the points are tileable.
+
+``` js
+const ps = new Poissimple({
+    n: 200,
+    dimensions: 2,
+    extent: [[-1, 1], [0, 1]],
+    periodic: true
+});
+console.log(ps.fill()); // Outputs array of arrays: [[x1, y1], [x2, y2], ...]
+```
+<center>
+<img src="./docs/img/2d-example.png" width="70%"></img>
+</center>
+
+### **Three-dimensional points:**
+If `extent` is not specified, the coordinate bounds will be [-1, 1] along each dimension.
+In the 3D case, the default extent is the cube [-1, 1] × [-1, 1] × [-1, 1].
+Below we sample 400 points in this cube and plot them, letting the color vary with the z-coordinate.
+
+
+``` js
+const ps = new Poissimple({
+    n: 400,
+    dimensions: 3,
+});
+console.log(ps.fill()); // Outputs array of arrays: [[x1, y1, z1], [x2, y2, z2], ...]
+```
+
+<center>
+<img src="./docs/img/3d-example.png" width="70%"></img>
+</center>
+
+## API
+
+### Constructor
+
+**new Poissimple(options[, rng])**
+
+- *options :*
+  - *n :* The number of points to generate, required
+  - *dimensions :* The dimensionality of the points, defaults to 2
+  - *extent :* The extent of the space to sample points from. For one-dimensional sampling, specify an array containing a lower and upper value (e.g. [-1, 1]). Otherwise, specify an array containing one array per dimension (e.g. [[-1, 1], [0, 1]] for the rectangle [-1, 1] × [0, 1]). Defaults to an extent with lower bound -1 and upper bound 1 along each dimension
+  - *periodic :* Specifies whether the distance between two points should be measured as if opposite boundaries are "glued together"/periodic. For example: If the extent is [0, 1] (1D), the distance between 0.1 and 0.9 is 0.8 when `periodic = false`, but 0.2 when `periodic = true`. Leads to tileable points. Either a boolean or an array with one boolean per dimension, defaults to false
+  - *tries :* The number of points generated before the algorithm gives up and uses the best candidate, defaults to 30
+- *rng :* The random number generator function used when generating points, defaults to Math.random
+
+``` js
+// Samples 50 1D values
+const ps = new Poissimple({
+    n: 50,
+    dimensions: 1,
+    extent: [[-1, 1], [0, 1]],
+    periodic: false,
+    tries: 15
+});
+```
+
+``` js
+// Samples 100 2D points that are tileable along x-axis using custom RNG
+const customRng = createRng(seed);
+const ps = new Poissimple({
+    n: 100,
+    dimensions: 2,
+    extent: [[-1, 1], [0, 1]],
+    periodic: [true, false],
+    tries: 15
+}, customRng);
+```
+
+### Method
+
+**ps.addPoint(point)**
+
+Manually adds a point to the object.
+Ensures that the value of `point` has the correct dimensions, but does not check distance to existing points.
+
+``` js
+ps.addPoint([0.2, 0.4]);
+```
+
+**ps.next()**
+
+Generates and returns the next point.
+If `n` points have already been generated, the returned value is `null`.
+``` js
+const point = ps.next();
+console.log(point);
+```
+
+**ps.fill()**
+
+Generates and fills the object with `n` points, which are returned as an array.
+
+``` js
+const points = ps.fill();
+```
+
+**ps.getPoints()**
+
+Returns an array containing the generated points.
+
+``` js
+const points = ps.getPoints();
+```
--- src/poissimple.js
+++ src/poissimple.js
@@ -9,14 +9,41 @@
  * @param {int} options.n The number of points to generate
  * @param {object} [options.dimensions] The dimensionality of the points, defaults to 2
  * @param {Array|Array[]} [options.extent] The extent of the space to sample from, must be either an array of two numbers (1D) or an array of arrays (>=2D), defaults to "volume" bounded between -1 and 1 along each axis
+ * @param {boolean|Array} [options.periodic] This parameter specifies whether the algorithm should generate tileable/periodic points. Either a boolean or an array of booleans
  * @param {int} [options.tries] The number of points generated before the algorithm gives up and uses the best candidate, defaults to 30
- * @param {boolean} [options.repulsiveBoundary] If true, the algorithm ensures that the sampled positions are at a distance from the boundary of the extent (half the distance used between positions), defaults to false
  * @param {function} [rng] The RNG function used when generating points, defaults to Math.random
  * @constructor
  */
 function Poissimple(options, rng = Math.random) {
-    this.n = options.n || 10;
-    this.dim = options.dimensions || 2;
+    if (typeof options.n !== "number" || options.n < 1) {
+        throw new Error("'n' must be an integer greater than 0")
+    }
+    this.n = options.n;
+
+    // Dimensions specified by user
+    const specifiedDim = options.dimensions;
+    // Dimensions inferred from extent parameter
+    const inferredDim = options.extent === undefined ? undefined :
+                        typeof options.extent[0] === "number" ? 1 :
+                        options.extent.length;
+
+    if (specifiedDim < 1) throw new Error("'dimensions' must be an integer greater than or equal to 1");
+
+    // Check: Are both dimensions and extent unspecified, or inconsistent?
+    // This is a bit hard to follow, should be refactored
+    if (specifiedDim === undefined && inferredDim === undefined) {
+        throw new Error("At least one of 'dimensions' and 'extent' must be specified");
+    } else if (typeof specifiedDim === "number" && typeof inferredDim === "number" && specifiedDim !== inferredDim) {
+        throw new Error("The parameters 'dimensions' and 'extent' are inconsistent");
+    } else if (specifiedDim === undefined && inferredDim !== undefined) {
+        // If 'dimensions' is unspecified, determine from 'extent'
+        this.dim = typeof options.extent[0] === "number" ? 1 : options.extent.length;
+    } else if (specifiedDim !== undefined) {
+        this.dim = specifiedDim;
+    }
+
+    // If the above checks are fruitless, set to default value 2
+    this.dim =  this.dim || 2;
 
     if (this.dim === 1 && options.extent) {
         this.extent = [options.extent];
@@ -26,19 +53,21 @@ function Poissimple(options, rng = Math.random) {
     
     this.tries = options.tries || 30;
     this.rng = rng;
-    this.repulsiveBoundary = options.repulsiveBoundary || false;
 
+    const periodic = options.periodic || false;
+    // this.periodic should be on format [boolean, boolean,...] with one entry per dimension
+    if (typeof periodic === "boolean") {
+        this.periodicArray = Array(this.dim).fill().map(() => periodic);
+    } else if (Array.isArray(periodic)) {
+        this.periodicArray = periodic;
+    } else {
+        throw new Error("'periodic' must either be a boolean or an array");
+    }
+    this.isPeriodic = this.periodicArray.some(v => v);
+    
     this._computeExtentVolume();
     this._computeLowerDistance();
     this.points = [];
-}
-
-/**
- * Computes the minimum allowed distance between generated points.
- */
-Poissimple.prototype._computeLowerDistance = function() {
-    // This heuristic works well for dimensions 1, 2, and 3, no guarantees above that
-    this.radius = Math.PI / 4 * Math.pow(this.volume / this.n, 1 / this.dim);
 }
 
 /**
@@ -54,6 +83,14 @@ Poissimple.prototype._computeExtentVolume = function() {
 }
 
 /**
+ * Computes the minimum allowed distance between generated points.
+ */
+ Poissimple.prototype._computeLowerDistance = function() {
+    // This heuristic works well for dimensions 1, 2, and 3, no guarantees above that
+    this.radius = Math.pow(0.6169 * this.volume / this.n, 1 / this.dim);
+}
+
+/**
  * Generates a uniformly sampled position from the extent.
  * @returns {Array} The generated point, represented as an array with length equal to the number of dimensions
  */
@@ -62,13 +99,52 @@ Poissimple.prototype._sampleUniform = function() {
 }
 
 /**
- * Computes the distance between two points.
+ * Helper function that computes distance between two points (either standard or periodic).
  * @param {Array} point1 The first point
  * @param {Array} point2 The second point
  * @returns {float} The distance between the two points
  */
 Poissimple.prototype._distance = function(point1, point2) {
+    if (this.dim === 1) {
+        point1 = (typeof point1 === "number") ? [point1] : point1;
+        point2 = (typeof point2 === "number") ? [point2] : point2;
+    }
+    return this.isPeriodic ? 
+        this._periodicDistance(point1, point2) : 
+        this._standardDistance(point1, point2);
+}
+
+/**
+ * Computes the distance between two points.
+ * @param {Array} point1 The first point
+ * @param {Array} point2 The second point
+ * @returns {float} The distance between the two points
+ */
+Poissimple.prototype._standardDistance = function(point1, point2) {
     const diffArray = Array(this.dim).fill().map((_, i) => point2[i] - point1[i]);
+    return Math.hypot(...diffArray);
+}
+
+/**
+ * Computes the periodic distance between two points.
+ * @param {Array} point1 The first point
+ * @param {Array} point2 The second point
+ * @returns {float} The distance between the two points
+ */
+Poissimple.prototype._periodicDistance = function(point1, point2) {
+    const ext = this.extent;
+    const per = this.periodicArray;
+
+    let diffArray = Array(this.dim).fill().map((_, i) => {
+        let d = point2[i] - point1[i];
+        
+        if (per[i]) {
+            const width = ext[i][1] - ext[i][0];
+            d = Math.abs(d);
+            d = Math.min(d, width - d);
+        }
+        return d;
+    });
     return Math.hypot(...diffArray);
 }
 
@@ -87,17 +163,16 @@ Poissimple.prototype._distanceToExisting = function(candidate) {
 }
 
 /**
- * Computes the distance between a candidate position and the boundary of the extent.
- * @param {Array} candidate 
- * @returns The distance to the boundary
+ * Manually adds a point to the object.
+ * @param {float|Array} point The point to add, either a number (1D) or an array of numbers (>1D)
  */
-Poissimple.prototype._distanceToBoundary = function(candidate) {
-    let minDistance = Infinity;
-    candidate.forEach((coordinate, i) => {
-        const [lower, upper] = this.extent[i];
-        minDistance = Math.min(minDistance, Math.abs(coordinate - lower), Math.abs(upper - coordinate));
-    });
-    return minDistance;
+Poissimple.prototype.addPoint = function(point) {
+    if (this.dim === 1 && typeof point !== "number") {
+        throw new Error("'point' should be a number when sampling in 1D");
+    } else if (this.dim > 1 && point.length !== this.dim) {
+        throw new Error("'point' has incorrect dimensions");
+    }
+    this.points.push(point);
 }
 
 /**
@@ -118,11 +193,6 @@ Poissimple.prototype.next = function() {
         const candidate = this._sampleUniform();
         let distToNearest = this._distanceToExisting(candidate);
 
-        if (this.repulsiveBoundary) {
-            // Multiply by 2 because distance to boundary must be larger than this.radius / 2
-            distToNearest = Math.min(distToNearest, 2*this._distanceToBoundary(candidate));
-        }
-
         if (distToNearest > largestDist) {
             bestPoint = candidate;
             largestDist = distToNearest;
@@ -130,7 +200,8 @@ Poissimple.prototype.next = function() {
         if (distToNearest > this.radius) break;
         numTries++;
     }
-    this.points.push(bestPoint);
+    // If 1D, push value instead of array
+    this.points.push(this.dim === 1 ? bestPoint[0] : bestPoint);
     return bestPoint;
 }
 
